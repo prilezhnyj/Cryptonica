4c44e7126c0c82d233c146abb771620d4fa3a8ff
Maksim Botalov <dev.botalov@gmail.com>
2023-09-27 12:04:28 +0500

    Initial Commit

---
+++ Cryptonica/CryptonicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptonicaApp.swift
+//  Cryptonica
+//
+//  Created by Максим Боталов on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptonicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
